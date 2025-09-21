e4f091799ceadd2f8577f08b049a8f4e0611af9b
Maxime Lombard <97088788+Zypheone94@users.noreply.github.com>
2025-09-21 14:15:50 +0200

    [CRY-14] : Angular auto refresh (#8)
    * [EVO] : add rxjs to subscribe for an action (decide by time)
    
    Clean angular project files
    
    * [FIX] : fix angular build version to match with attended
    
    fix messages from french to english

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Analytics } from './analytics/analytics';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "home", component: Home, title: "Crypto-Viz"},
+    {path: "analytics", component: Analytics, title: "Analytics"},
+    {path: "**", redirectTo: "/home", pathMatch: "full"}
+];
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import 'zone.js'
 import { App } from './app/app';
+import { Home } from './app/home/home';
+import { Analytics } from './app/analytics/analytics';
+import { provideRouter, Routes } from '@angular/router';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+  const routes: Routes = [
+    { path: "", redirectTo: "/home", pathMatch: 'full' },
+    { path: "home", component: Home, title: "Crypto-Viz"},
+    { path: "analytics", component: Analytics, title: "Analytics"},
+    { path: "**", redirectTo: "/home", pathMatch: 'full'}
+  ];
+
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)]
+})
