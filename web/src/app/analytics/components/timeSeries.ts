3fa65f367d15969250918e12fe19ea123c40ebc3
Maxime Lombard <maximerene.durand.pro@gmail.com>
2025-09-24 16:07:24 +0200

    [EVO] : Implements basic Angular Material setup
    Adds Angular Material and CDK dependencies to the project.
    Configures a custom theme using a new `custom-theme.scss` file and includes Roboto font.
    Removes the default content from index.html and creates a basic TimeSeries component.

---
+++ web/src/app/analytics/components/timeSeries.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-time-series',
+    templateUrl: 'timeSeries.html'
+})
+
+export class TimeSeries implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
