00552131de61d70c4531c58a88f7679d74752020
Maxime Lombard <maximerene.durand.pro@gmail.com>
2025-09-24 15:16:37 +0200

    [EVO] : Adds health check endpoint and UI
    Introduces a health check endpoint to monitor the application's status.
    
    The UI now includes a health check view, displaying the application's
    uptime and version information.

---
+++ web/src/app/health-check/health-check.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from "../services/api.service";
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-health-check',
+  imports: [CommonModule],
+  standalone: true,
+  providers: [ApiService],
+  templateUrl: './health-check.html',
+  styleUrls: ['./health-check.css']
+})
+export class HealthCheck implements OnInit {
+  upTime: any;
+  version: any;
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+     this.api.getHealthCheck().subscribe((data) => {
+      this.upTime = data.upTime;
+      this.version = data.version;
+      console.log(data);
+    });
+  }
+
+
+}
---
+++ web/src/app/services/api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root' 
+})
+export class ApiService {
+  private baseUrl = 'http://localhost:8080'; 
+
+  constructor(private http: HttpClient) {}
+
+  getHealthCheck(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/health/check`);
+  }
+}
