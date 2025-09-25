58dff2896472d81cf2992b2d4c3876fc8d913c2c
Maxime Lombard <maximerene.durand.pro@gmail.com>
2025-09-25 21:19:22 +0200

    [EVO] : Refactors application layout and routing
    Improves application structure by introducing a main layout component with navigation and filter sidebar.
    Updates routing configuration to accommodate the new layout.
    Migrates components to be organized within dedicated directories.
    Adds ng2-charts library for data visualization.

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,28 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
-  template: `
-  <nav>
-      <a routerLink="/home" 
-         routerLinkActive="active">Accueil</a>
-      <a routerLink="/analytics" 
-         routerLinkActive="active">Analytics</a>
-      <a routerLink="/health-check" 
-         routerLinkActive="active">Health Check</a>
-    </nav>
-    
-    <router-outlet></router-outlet>
-  `,
-  styles: [`
-    .active { 
-      font-weight: bold; 
-      color: #007acc; 
-    }
-  `]
+  imports: [RouterOutlet],
+  template: ` <router-outlet></router-outlet> `,
+  styles: [
+    `
+      .active {
+        font-weight: bold;
+        color: #007acc;
+      }
+    `,
+  ],
 })
-
 export class App {}
---
+++ web/src/app/component/analytics/components/time-series.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+import { ComponentState } from '../../../shared/enums/component-state.enum';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CommonModule } from '@angular/common';
+
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartOptions, ChartType } from 'chart.js';
+
+@Component({
+  selector: 'app-time-series',
+  standalone: true,
+  imports: [MatProgressSpinnerModule, CommonModule, BaseChartDirective],
+  templateUrl: 'time-series.html',
+})
+export class TimeSeries implements OnInit {
+  componentState = ComponentState;
+  currentState: ComponentState = ComponentState.LOADING;
+  errorMessage = '';
+
+  data: any[] = [];
+
+  // mokedData
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        fill: true,
+        tension: 0.5,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)',
+      },
+    ],
+  };
+
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: false,
+  };
+  public lineChartLegend = true;
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.api.getTimeseries().subscribe((data) => {
+      this.data = data;
+      console.log('Time Series Data:', data);
+    });
+  }
+}
