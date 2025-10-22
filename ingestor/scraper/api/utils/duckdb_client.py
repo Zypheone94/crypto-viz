ba8877ae5b8f397938cdd806b5dc51b0254204af
Maxime Lombard <maximerene.durand.pro@gmail.com>
2025-10-22 19:24:06 +0200

    [FEAT] : remake writing parquet into db

--- ingestor/scraper/api/utils/duckdb_client.py
+++ ingestor/scraper/api/utils/duckdb_client.py
@@ -1,4 +1,11 @@
 import duckdb
-import os
+from pathlib import Path
 
-parquets_dir = os.path.join(os.path.dirname(__file__), "parquets")
+PARQUET_PATH = Path(__file__).parent.parent.parent.parent / "data" / "clean" / "parquet"
+
+db = duckdb.connect(Path(__file__).parent.parent.parent / "data" / "duck" / "warehouse.duckdb")
+
+for filename in PARQUET_PATH.glob("*.parquet"):
+    table = filename.stem
+
+    db.execute(f"INSERT INTO {table} SELECT * FROM read_parquet('{filename}')")
