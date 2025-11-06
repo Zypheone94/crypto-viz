43b6824f10a44fbcba7e23f7d11e1d50d0e0788e
Maxime Lombard <maximerene.durand.pro@gmail.com>
2025-11-06 10:41:59 +0100

    [DATABASE] : change database to sqlite

---
+++ ingestor/scraper/component/scraperdb/init_sqlite_db.py
@@ -0,0 +1,50 @@
+import sqlite3
+from pathlib import Path
+import os
+
+# Assure-toi que le dossier 'data' existe
+Path("data").mkdir(exist_ok=True)
+
+con = sqlite3.connect("data/ingestor.db")
+cur = con.cursor()
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS symbol (
+    id INTEGER PRIMARY KEY,
+    symbol TEXT UNIQUE NOT NULL
+);
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS delta (
+    id INTEGER PRIMARY KEY,
+    symbol TEXT NOT NULL,
+    date_start TIMESTAMP,
+    date_end DATE,
+    window_label TEXT,
+    delta FLOAT,
+    delta_pct FLOAT,
+    FOREIGN KEY(symbol) REFERENCES symbol(symbol)
+);
+''')
+
+cur.execute('''
+CREATE TABLE IF NOT EXISTS article (
+    id INTEGER PRIMARY KEY,
+    date DATE,
+    titre TEXT,
+    url TEXT,
+    source TEXT,
+    symbol TEXT NOT NULL,
+    name TEXT,
+    price FLOAT,
+    market_cap FLOAT,
+    coin_circulating FLOAT,
+    FOREIGN KEY(symbol) REFERENCES symbol(symbol)
+);
+''')
+
+con.commit()
+
+print("✅ Tables créées :", list(cur.execute("SELECT name FROM sqlite_master WHERE type='table';")))
+con.close()
