3e3d516e8edffc4bf0811ae06e64cae76b193813
Ultiorra <jarreaumik@hotmail.com>
2025-11-14 12:32:12 +0100

    fix builder according to windowed script

--- ingestor/builder/consumer.py
+++ ingestor/builder/consumer.py
@@ -91,7 +91,6 @@ def flush_batch(batch: List[Dict]) -> int:
         .dt.convert_time_zone("UTC")
         .alias("fetched_ts"),
     ])
-
     valid = (
         df.with_columns([
             pl.col("title").cast(pl.Utf8).map_elements(_clean_text, return_dtype=pl.Utf8),
@@ -103,16 +102,19 @@ def flush_batch(batch: List[Dict]) -> int:
             pl.col("market_cap_usd").cast(pl.Float64, strict=False),
             pl.col("volume_24h").cast(pl.Float64, strict=False),
             pl.col("coin_circulating").cast(pl.Float64, strict=False),
+
             pl.when(pl.col("fetched_ts").is_not_null())
-              .then(pl.col("fetched_ts").dt.date().cast(pl.Utf8))
-              .otherwise(pl.lit("unknown"))
-              .alias("date"),
+            .then(pl.col("fetched_ts").dt.date().cast(pl.Utf8))
+            .otherwise(pl.lit("unknown"))
+            .alias("date"),
         ])
         .select([
             "id", "title", "url", "source",
             "published_at", "fetched_at",
             "symbol", "price_usd", "market_cap_usd", "volume_24h", "coin_circulating",
+            "ts",
             "date",
+            "fetched_ts"
         ])
     )
 
